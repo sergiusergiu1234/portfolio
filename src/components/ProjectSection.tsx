ca360015e2962a310ea4f7280318924d37f625a5
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2023-11-21 13:12:41 +0200

    first commit

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -35,8 +35,8 @@ const handleNavToGithub =() =>{
         </div>
        
        <div className="image-container">
-            <img src={process.env.PUBLIC_URL + '/online-sop3.jpeg'} alt="Profile Picture" className="image-box"/>   
-            <img src={process.env.PUBLIC_URL + '/online-shop.jpeg'} alt="Profile Picture" className="image-box image-overlay"/>    
+            <img src={'./online-sop3.jpeg'} alt="Profile Picture" className="image-box"/>   
+            <img src={'./online-shop.jpeg'} alt="Profile Picture" className="image-box image-overlay"/>    
         </div>
     </div>)
 }
