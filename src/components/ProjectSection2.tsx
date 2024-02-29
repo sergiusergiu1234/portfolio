27ca64208f21a755ac3a1db21da80750ab7345ef
STEfAN SERGIU <sergiuc.stefan@gmail.com>
2024-02-29 15:24:41 +0200

    update

--- src/components/ProjectSection2.tsx
+++ src/components/ProjectSection2.tsx
@@ -1,9 +1,15 @@
-const ProjectSection2 = ()=>{
-    return (<div className="project-section-container">
+import { Button } from "./Button";
 
-    <video className="video-container" src={"./chat-showcase.mp4"} width="600" height="300" controls autoPlay />
-        <div>
-            <h4>Real-time chat application sample</h4>
+const ProjectSection2 = ()=>{
+    return (<div className="project2-section-container">
+    
+             <h4>Real-time chat application sample</h4>  
+        
+   
+        <div className="project2-container">
+            <video className="video-container" src={"./chat-showcase.mp4"} width="600" height="300" controls autoPlay />
+            <div className="project2-description"   >
+            
             <p className="project-description">
                 This is a chat application similar to Whatsapp or Telegram.
                 <br/><br/>
@@ -11,12 +17,17 @@ const ProjectSection2 = ()=>{
                  providing insights into the complexities of real-time communication and
                 the nuances of working with a NoSQL database like MongoDB. 
                 <br/><br/>
+                <button className="project2-button">Visit Github repository</button> 
+                <br/><br/>
                  While the project has achieved its intednded purpose, there are plans to
                incorporate new features and improvements in the future.
-
             </p>
+            
+            </div>
+               
+            
         </div>
-   
+        
     </div>)
 }
 
